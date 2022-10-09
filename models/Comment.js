755d93ef6ebbe3bdb4a1e111e0cc657f18b8a354
Jorge Garit <jorgegarit@gmail.com>
2022-10-09 17:53:08 -0400

    added init function to comment model

--- models/Comment.js
+++ models/Comment.js
@@ -5,4 +5,40 @@ const sequelize = require('../config/connection');
 class Comment extends Model {}
 
 // creating columns and fields for Comment model
-
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1] //comment must be at least 1 character long
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
