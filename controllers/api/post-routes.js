d441338bbca56e4341ec610ea37f901a3bced649
Jorge Garit <jorgegarit@gmail.com>
2022-10-09 18:54:14 -0400

    added request to get all post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,3 +2,32 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
+
+// get all post 
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        attributes: ['id', 'title', 'post_content', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            // comment model
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                  model: User,
+                  attributes: ['username']
+                } 
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(error => {
+        console.log(error);
+        res.status(500).json(error);
+    });
+})
