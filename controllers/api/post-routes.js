900254d19ae435a5a3afb8640379d24349f694e2
Jorge Garit <jorgegarit@gmail.com>
2022-10-09 19:00:45 -0400

    users can get post by specific id

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -19,6 +19,7 @@ router.get('/', (req, res) => {
                   attributes: ['username']
                 } 
             },
+            // user model
             {
                 model: User,
                 attributes: ['username']
@@ -31,3 +32,38 @@ router.get('/', (req, res) => {
         res.status(500).json(error);
     });
 })
+
+// get post by id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'title', 'post_content', 'created_at'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found for this id' });
+          return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json(error);
+    });
+  });
