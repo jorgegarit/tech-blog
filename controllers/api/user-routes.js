28706436908d241d1e008869be3f384dfc8b18c6
Jorge Garit <jorgegarit@gmail.com>
2022-10-09 18:17:41 -0400

    can now search specific user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,3 +12,38 @@ router.get('/', (req, res) => {
         res.status(500).json(error);
     });
 });
+
+// find specific user by id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_content', 'created_at'] 
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user was found with this id'})
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json(error);
+    });
+});
