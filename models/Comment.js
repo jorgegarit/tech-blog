5f51c8032926fe62de67eaf534b615817fd91c45
Jorge Garit <jorgegarit@gmail.com>
2022-10-09 17:48:27 -0400

    added comment model

---
+++ models/Comment.js
@@ -0,0 +1,8 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create the Comment model
+class Comment extends Model {}
+
+// creating columns and fields for Comment model
+
