c23ae8d8d4ad6b012e1a0583f8ecf524d8a180c4
Jorge Garit <jorgegarit@gmail.com>
2022-10-09 17:39:52 -0400

    added user.init fields for user model

--- models/User.js
+++ models/User.js
@@ -11,3 +11,54 @@ class User extends Model {
         return bcrypt.compareSync(loginPW, this.password);
     }
 }
+
+// creating columns and fields for User model
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allownull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true, //this means that every user most have a unique email
+            validate: {
+                isEmail: true //will validate that it is in email format "blank@blank.com"
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8] //password must be atleast 8 characters
+            }
+        }
+    },
+    {
+        hooks: {
+            // set up the hook functionality before password is created
+            async beforeUserCreated(newUser) {
+                newUser.password = await bcrypt.hash(newUser.password, 10);
+                return newUser;
+            },
+
+            async beforeUserUpdate(updateUser) {
+                updateUser.password = await bcrypt.hash(updateUser.password, 10);
+                return updateUser;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
