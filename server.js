bd2df40102ea8a0a77c53f5579790a9bf3837fdd
Jorge Garit <jorgegarit@gmail.com>
2022-10-09 20:24:29 -0400

    set up helper and created server.js so that backend can be tested

---
+++ server.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const helpers = require('./utils/helpers')
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: process.env.secret,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({db: sequelize})
+};
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening!'));
+});
